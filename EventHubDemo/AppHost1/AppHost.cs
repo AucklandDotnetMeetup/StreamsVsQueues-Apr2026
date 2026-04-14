ac7756ab8903d6c1cd9f8dcfb55e7a39803681cc
Bo Long <zjgslongbo1@gmail.com>
2026-04-14 22:09:50 +1200

    uncomment

--- EventHubDemo/AppHost1/AppHost.cs
+++ EventHubDemo/AppHost1/AppHost.cs
@@ -4,11 +4,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Emulator>("Emulator");
 builder.AddProject<Projects.Producers>("Producers");
-//builder.AddProject<Projects.ReceiverNotSendOffset>("ReceiverNotSendOffset").WithExplicitStart();
+builder.AddProject<Projects.ReceiverNotSendOffset>("ReceiverNotSendOffset").WithExplicitStart();
 builder.AddProject<Projects.ReceiverSendOffsetReplica1>("ReceiverSendOffsetReplica1").WithExplicitStart(); ;
 builder.AddProject<Projects.ReceiverSendOffsetReplica2>("ReceiverSendOffsetReplica2").WithExplicitStart(); ;
-//builder.AddProject<Projects.ReceiverWithKafkaReplica1>("ReceiverWithKafkaReplica1").WithExplicitStart(); ;
-//builder.AddProject<Projects.ReceiverWithKafkaReplica2>("ReceiverWithKafkaReplica2").WithExplicitStart(); ;
-//builder.AddProject<Projects.ReceiverWithProcessorBatch>("ReceiverWithProcessorBatch").WithExplicitStart(); ;
+builder.AddProject<Projects.ReceiverWithKafkaReplica1>("ReceiverWithKafkaReplica1").WithExplicitStart(); ;
+builder.AddProject<Projects.ReceiverWithKafkaReplica2>("ReceiverWithKafkaReplica2").WithExplicitStart(); ;
+builder.AddProject<Projects.ReceiverWithProcessorBatch>("ReceiverWithProcessorBatch").WithExplicitStart(); ;
 
 builder.Build().Run();
